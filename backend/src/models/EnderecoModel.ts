1efe4901797eccdc4819a8e5c447b18c6f478f9d
Jayane <176343420+jayane-c@users.noreply.github.com>
2025-11-28 23:50:58 -0300

    feat(EnderecoModel): adicionada a entidade de Endereco com mapeamento TypeORM.

---
+++ backend/src/models/EnderecoModel.ts
@@ -0,0 +1,36 @@
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne} from "typeorm";
+
+@Entity("endereco")
+export class EnderecoModel {
+
+    @PrimaryGeneratedColumn({name:'id_endereco'})
+    id_endereco!: number;
+
+    @Column({length: 10, nullable: false})
+    cep!: string;
+
+    @Column({length: 255, nullable: false})
+    logradouro!: string;
+
+    @Column({length: 10, nullable: false})
+    numero!: string;
+
+    @Column({length: 100, nullable: false})
+    complemento!: string;
+
+    @Column({length: 100, nullable: false})
+    bairro!: string;
+
+    @Column({length: 100, nullable: false, default: 'Igarassu'})
+    cidade!: string;
+
+    @Column({type: 'char', length: 2, nullable: false, default: 'PE'})
+    estado!: string;
+
+    @Column({type: 'float', nullable: false, name:'lat_endereco'})
+    latitude!: number;
+
+    @Column({type: 'float', nullable: false, name: 'lon_endereco'})
+    longitude!: number;
+}
+
