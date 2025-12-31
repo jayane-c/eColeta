e8f4bca10394e3d865c417bbf736e9789ec73b7a
Debora Milena <deboramilenasn@gmail.com>
2025-12-30 23:22:26 -0300

    feat: ajustes no dashboard do morador

--- frontend/src/pages/dashboard/morador/DashboardMorador.tsx
+++ frontend/src/pages/dashboard/morador/DashboardMorador.tsx
@@ -1,17 +1,19 @@
 import './DashboardMorador.css';
 import NavbarMorador from '../../../Components/navbar-morador/NavbarMorador';
 import DashboardContentMorador from '../../../Components/dashboardContent/DashboardContentMorador';
+import HistoricoMorador from '../../../Components/HistoricoMorador/HistoricoMorador';
 
 export default function DashboardMorador() {
- 
-    const nomeDoUsuarioLogado = "Débora"; 
+  const nomeDoUsuarioLogado = "Débora";
 
-    return (
-        <div className="recipiente-dashboard-principal">
-           
-            <NavbarMorador nome={nomeDoUsuarioLogado} />
-            
-            <DashboardContentMorador />
-        </div>
-    );
+  return (
+    <div className="recipiente-dashboard-principal">
+      <NavbarMorador nome={nomeDoUsuarioLogado} />
+      
+      <div className="conteudo-central">
+        <DashboardContentMorador /> 
+        <HistoricoMorador />
+      </div>
+    </div>
+  );
 }
