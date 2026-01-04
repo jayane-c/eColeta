240ae1faacdd58f104c880a83522636e03192d91
Debora Milena <deboramilenasn@gmail.com>
2026-01-04 00:32:28 -0300

    refactor: sincroniza cadastro e navegação com sistema de histórico

--- frontend/src/pages/dashboard/morador/DashboardMorador.tsx
+++ frontend/src/pages/dashboard/morador/DashboardMorador.tsx
@@ -4,23 +4,18 @@ import DashboardContentMorador from '../../../Components/dashboardContent/Dashbo
 import HistoricoMorador from '../../../Components/HistoricoMorador/HistoricoMorador';
 import Footer from '../../../Components/Footer/footer';
 
-
 export default function DashboardMorador() {
-    const nomeDoUsuarioLogado = "Débora";
 
     return (
         <div className="recipiente-dashboard-principal">
-            <NavbarMorador nome={nomeDoUsuarioLogado} />
+            <NavbarMorador />
 
             <div className="conteudo-central">
                 <DashboardContentMorador />
                 <HistoricoMorador />
-
             </div>
-            <div>
-                <Footer />
-            </div>
-
+            
+            <Footer />
         </div>
     );
 }
