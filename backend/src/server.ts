a854210709228c7e99480b2618706244b594202e
matheus-nbx52 <matheusfssilva2001@gmail.com>
2026-01-04 18:16:39 -0300

    feat(cors): Adiciona CORS ao backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { initializeDatabase } from './config/database';
 import authRoutes from './routes/AuthRoutes';
 import userMoradorRoutes from './routes/UserMoradorRoutes';
 import ResiduoRoutes from "./routes/ResiduoRoutes";
 import { coletaRoutes } from './routes/ColetaRoutes';
 
-
 // Middleware para parsear JSON
 const app = express();
 app.use(express.json());
+
+// cors
+app.use(cors());
 
 // Rotas de autenticação
 app.use('/auth', authRoutes);
