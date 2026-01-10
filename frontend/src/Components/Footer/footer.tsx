aea988fb0304df04e1b1c9e221beeb5bdb3030da
Debora Milena <deboramilenasn@gmail.com>
2026-01-09 23:12:27 -0300

    feat: adiciona links de navegação ao footer

--- frontend/src/Components/Footer/footer.tsx
+++ frontend/src/Components/Footer/footer.tsx
@@ -1,4 +1,5 @@
 import "./Footer.css";
+import { Link } from "react-router-dom";
 import { MapPin, Mail, Phone } from "lucide-react";
 
 export default function Footer() {
@@ -18,26 +19,36 @@ export default function Footer() {
             Transforme seus resíduos em impacto positivo.
           </p>
 
-          <div className="footer-selo">
-            ♻️ Certificado Selo Verde
-          </div>
+          <div className="footer-selo">♻️ Certificado Selo Verde</div>
         </div>
 
         <div className="footer-links">
           <h4>Links Rápidos</h4>
           <ul>
-            <li>Como Funciona</li>
-            <li>Guia de Separação</li>
-            <li>Área do Usuário</li>
+            <li>
+              <Link to="/saibaMais">Como Funciona</Link>
+            </li>
+            <li>
+              <Link to="/guia-separacao">Guia de Separação</Link>
+            </li>
+            <li>
+              <Link to="/login">Área do Usuário</Link>
+            </li>
           </ul>
         </div>
 
         <div className="footer-links">
           <h4>Junte-se a Nós</h4>
           <ul>
-            <li>Cadastro de Morador</li>
-            <li>Cadastro de Coletor</li>
-            <li>Cadastro de Cooperativa</li>
+            <li>
+              <Link to="/cadastro-morador">Cadastro de Morador</Link>
+            </li>
+            <li>
+              <Link to="/cadastro-coletor">Cadastro de Coletor</Link>
+            </li>
+            <li>
+              <Link to="/cadastro-cooperativa">Cadastro de Cooperativa</Link>
+            </li>
           </ul>
         </div>
 
@@ -51,7 +62,7 @@ export default function Footer() {
 
           <div className="contato-item">
             <Phone size={18} />
-            <span>(81) 99636-3918 </span>
+            <span>(81) 99636-3918</span>
           </div>
 
           <div className="contato-item">
